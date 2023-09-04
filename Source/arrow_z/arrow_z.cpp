cf0ab602f7ac115ddf6cc7c2226c7c28f225b494
parth2860 <parth2837@gmail.com>
2023-09-04 21:01:27 +0530

    init_reversed

--- Source/arrow_z/arrow_z.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "arrow_z.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, arrow_z, "arrow_z" );
--- Source/arrow_z/arrow_zGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "arrow_zGameModeBase.h"
-
