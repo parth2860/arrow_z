116e15332d6d2426e33b56ad50c4df89bafebbc9
parth2860 <parth2837@gmail.com>
2024-01-26 21:14:24 +0530

    delete : old project file (in compatible with ue version)

--- Source/op_world/op_world.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "op_world.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, op_world, "op_world" );
- 
