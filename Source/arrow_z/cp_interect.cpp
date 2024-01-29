c8acff4f90a7c03907db4adae58f33554b735181
parth2860 <parth2837@gmail.com>
2024-01-29 13:38:52 +0530

    added : file added (box component doesnt work)

---
+++ Source/arrow_z/cp_interect.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "cp_interect.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+Acp_interect::Acp_interect()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//box_area = CreateDefaultSubobject<UBoxComponent>(TEXT("box_area"));
+	//BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	//RootComponent = BoxCollision;
+}
+
+// Called when the game starts or when spawned
+void Acp_interect::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Acp_interect::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/arrow_z/cp_interect.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "cp_interect.generated.h"
+
+UCLASS()
+class ARROW_Z_API Acp_interect : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	Acp_interect();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Box Components")
+		class BoxComponent* box_area;
+	UPROPERTY(VisibleAnywhere, Category = "Box Components")
+		class BoxComponent* BoxCollision;
+};
