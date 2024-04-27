920c6b9fa2368db11fad9e55999e4c933a68ccca
parth2860 <parth2837@gmail.com>
2024-04-27 18:58:23 +0530

    update : custom_notify added

---
+++ Source/arrow_z/MyAnimNotifyState_Trace.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "MyAnimNotifyState_Trace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARROW_Z_API UMyAnimNotifyState_Trace : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
