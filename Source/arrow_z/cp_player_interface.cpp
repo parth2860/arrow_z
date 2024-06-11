fbee19c0bf83e6f2139a583536eb1e2be734cee1
parth2860 <parth2837@gmail.com>
2024-06-11 20:56:38 +0530

    update : interface added

---
+++ Source/arrow_z/cp_player_interface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "cp_player_interface.h"
+
+// Add default functionality here for any Icp_player_interface functions that are not pure virtual.
---
+++ Source/arrow_z/cp_player_interface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "cp_player_interface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class Ucp_player_interface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ARROW_Z_API Icp_player_interface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
